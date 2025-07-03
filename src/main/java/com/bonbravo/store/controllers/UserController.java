fdc264f5fe5ad928270133b0c29fcc155bc8ae92
BravenAmuli <beamnggoblin254@gmail.com>
2025-07-04 01:18:25 +0300

    hashed Registration password and authorisation to some end points

--- src/main/java/com/bonbravo/store/controllers/UserController.java
+++ src/main/java/com/bonbravo/store/controllers/UserController.java
@@ -11,6 +11,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -25,6 +26,7 @@ public class UserController {
 
     private final UserRepository userRepository;
     private final UserMapper userMapper;
+    private final PasswordEncoder passwordEncoder;
 
 
 
@@ -53,12 +55,14 @@ public class UserController {
     @PostMapping
     public ResponseEntity<?> registerUser(
             UriComponentsBuilder uriBuilder,
-            @Valid @RequestBody RegisterUserRequest request) {
+            @Valid @RequestBody RegisterUserRequest request
+    ) {
 
         if(userRepository.existsByEmail(request.getEmail()))
             return ResponseEntity.badRequest().body(Map.of("message", "Email already exists."));
 
         var user  = userMapper.toEntity(request);
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         userRepository.save(user);
         var userDto = userMapper.toDto(user);
         var uri = uriBuilder.path("/users/{id}").buildAndExpand(userDto.getId()).toUri();
